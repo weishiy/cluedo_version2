8ed6f40fec8c18fa597c0fb7d8be4e594298c9b2
Jeremy Kanal-Scott <jkanalscott@gmail.com>
2023-08-17 15:15:27 +1200

    Added overloaded `render` for both the board and movement controls.

--- SWEN225_Assignment1/src/net/swen225/hobbydetectives/ui/view/GameUI.java
+++ SWEN225_Assignment1/src/net/swen225/hobbydetectives/ui/view/GameUI.java
@@ -3,6 +3,7 @@ package net.swen225.hobbydetectives.ui.view;
 import net.swen225.hobbydetectives.card.model.Card;
 import net.swen225.hobbydetectives.ui.bean.BoardBean;
 import net.swen225.hobbydetectives.ui.bean.ChooseCardBean;
+import net.swen225.hobbydetectives.ui.bean.MovementBean;
 import net.swen225.hobbydetectives.ui.bean.PauseMessageBean;
 
 import java.util.concurrent.Future;
@@ -20,7 +21,17 @@ public interface GameUI {
     void addController(Controller controller);
 
     /**
+     * Renders the game board using the given <code>boardBean</code> and <code>movementBean</code>.
+     *
+     * @param boardBean    The data used to render the board.
+     * @param movementBean The data for rendering the movement controls.
+     */
+    void render(BoardBean boardBean, MovementBean movementBean);
+
+    /**
      * Renders the game board using the given <code>boardBean</code>.
+     * <p>
+     * Doesn't allow for movement.
      *
      * @param boardBean The data used to render the board.
      */
