250d4b76bfd983073e41df31cc1deb985fc97a44
Jeremy Kanal-Scott <jkanalscott@gmail.com>
2023-08-19 17:01:17 +1200

    Removed unused imports.

--- SWEN225_Assignment1/src/net/swen225/hobbydetectives/ui/view/GameUI.java
+++ SWEN225_Assignment1/src/net/swen225/hobbydetectives/ui/view/GameUI.java
@@ -1,12 +1,5 @@
 package net.swen225.hobbydetectives.ui.view;
 
-import net.swen225.hobbydetectives.card.model.Card;
-import net.swen225.hobbydetectives.ui.bean.BoardBean;
-import net.swen225.hobbydetectives.ui.bean.ChooseCardBean;
-import net.swen225.hobbydetectives.ui.bean.PauseMessageBean;
-
-import java.util.concurrent.Future;
-
 /**
  * Manages the view of the game.
  */
